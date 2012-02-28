8f7b9cc3958b99a28ede009e45641098bd6ee45c
Jeremy Sawruk <jeremy.sawruk@gmail.com>
2012-02-28 10:50:25 -0500

    Read MP3 file into Numpy array

---
+++ pymir/audio/mp3.py
@@ -0,0 +1,26 @@
+# MP3.py
+# Functions for decoding MP3 into PCM using FFmpeg
+
+import os
+import struct
+from subprocess import Popen, PIPE
+
+import numpy
+from numpy import *
+
+def load(mp3File):
+    sampleRate = 44100
+
+    ffmpeg = Popen([
+            "ffmpeg",
+            "-i", mp3File,
+            "-vn", "-acodec", "pcm_s16le", # Little Endian 16 bit PCM
+            "-ac", "1", "-ar", str(sampleRate), # -ac = audio channels (1)
+            "-f", "s16le", "-"], # -f wav for WAV file
+            stdin = PIPE, stdout = PIPE, stderr = open(os.devnull, "w"))
+
+    rawData =  ffmpeg.stdout
+
+    mp3Array = numpy.fromstring(rawData.read(),numpy.int16)
+        
+    #print mp3Array.size
---
+++ readmp3.py
@@ -0,0 +1,4 @@
+# Example showing how to read mp3 files
+from pymir.audio import mp3
+
+mp3.load("audio_files/test-stereo.mp3")
